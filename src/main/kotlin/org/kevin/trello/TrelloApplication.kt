2596a442fa965f22c0d5a7bd6d9cc043a2e0026c
haomfeng <haomfeng@student.unimelb.edu.au>
2025-06-05 21:44:32 +1000

    init

---
+++ src/main/kotlin/org/kevin/trello/TrelloApplication.kt
@@ -0,0 +1,11 @@
+package org.kevin.trello
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TrelloApplication
+
+fun main(args: Array<String>) {
+	runApplication<TrelloApplication>(*args)
+}
