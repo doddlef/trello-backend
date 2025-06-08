6b697c91f78a1e0df6f8644ad9c216f7c0af7a6d
haomfeng <haomfeng@student.unimelb.edu.au>
2025-06-08 20:34:38 +1000

    feat: update response codes for unauthorized access and rename test class for clarity

--- src/main/kotlin/org/kevin/trello/account/controller/AccountController.kt
+++ src/main/kotlin/org/kevin/trello/account/controller/AccountController.kt
@@ -3,7 +3,6 @@ package org.kevin.trello.account.controller
 import org.kevin.trello.auth.utils.SecurityUtils
 import org.kevin.trello.core.response.ApiResponse
 import org.kevin.trello.core.response.ResponseCode
-import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -13,8 +12,6 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/account")
 class AccountController {
-    private val log = LoggerFactory.getLogger(this.javaClass)
-
     @GetMapping("/me")
     fun getAccountInfo(): ResponseEntity<ApiResponse> {
         val currentAccount = SecurityUtils.currentAccount()
